e9a9b003b17fef578fe5ec3f6175a99d3cfa5189
최태현 <choi-0202@kaist.ac.kr>
2022-09-13 20:39:15 +0900

    Basic Structure

---
+++ src/main/kotlin/com/group/kcd/KcdAccountMarketApplication.kt
@@ -0,0 +1,11 @@
+package com.group.kcd
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KcdAccountMarketApplication
+
+fun main(args: Array<String>) {
+  runApplication<KcdAccountMarketApplication>(*args)
+}
